a72b04cf3210db62328200942c740691f5691de9
Kasun Lakshitha <kasun2014@outlook.com>
2023-04-22 01:46:29 +0530

    html in node

--- node-js/node-js-app/app.js
+++ node-js/node-js-app/app.js
@@ -3,6 +3,13 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node js</title></head>');
+    res.write('<body>');
+    res.write('<h1>Hello we are node js</>');
+    res.write('</body>');
+    res.write('</html>');
 })
 
 server.listen(3000);
