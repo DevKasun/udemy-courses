70a3bfc560497ba6ca2bbd97354c992552308d29
Kasun Lakshitha <kasun2014@outlook.com>
2023-05-09 17:19:22 +0530

    Finished ejs

--- node-js/routes/admin.js
+++ node-js/routes/admin.js
@@ -7,7 +7,7 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { pageTitle: 'Add product', path: '/admin/add-product', productCSS: true, formCSS: true, isAddProductPageActive: true })
+  res.render('add-product', { pageTitle: 'Add product', path: '/admin/add-product', productCSS: true, formCSS: true, isAddProductPageActive: true });
 });
 
 // /admin/add-product => POST
